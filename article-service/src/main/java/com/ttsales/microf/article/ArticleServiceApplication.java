eb018d6ec35d06602f88c1a0a1c44efaf8330000
liyi <liyi.9ban@gmail.com>
2016-03-07 17:59:17 +0800

    init

--- article-service/src/main/java/com/ttsales/microf/article/ArticleServiceApplication.java
+++ article-service/src/main/java/com/ttsales/microf/article/ArticleServiceApplication.java
@@ -2,8 +2,10 @@ package com.ttsales.microf.article;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ArticleServiceApplication {
 
 	public static void main(String[] args) {
