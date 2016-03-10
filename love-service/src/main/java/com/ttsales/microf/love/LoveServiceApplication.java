58240b7fb4f685e28649f521c57339ed79f2bec9
liyi <liyi.9ban@gmail.com>
2016-03-10 13:56:16 +0800

    init

--- love-service/src/main/java/com/ttsales/microf/love/LoveServiceApplication.java
+++ love-service/src/main/java/com/ttsales/microf/love/LoveServiceApplication.java
@@ -8,20 +8,20 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.stereotype.Component;
 
 import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class LoveServiceApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(LoveServiceApplication.class, args);
 	}
-
-
 
 }
 @Component
