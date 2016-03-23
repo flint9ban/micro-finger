22bbf45def494df44d44c58c69d76072761628c3
zwt1990 <791431798@qq.com>
2016-03-23 17:55:43 +0800

    kk

--- love-client/src/main/java/com/ttsales/microf/love/keyword/domain/Keyword.java
+++ love-client/src/main/java/com/ttsales/microf/love/keyword/domain/Keyword.java
@@ -1,7 +1,9 @@
 package com.ttsales.microf.love.keyword.domain;
 
+import com.ttsales.microf.love.domainUtil.SuperEntity;
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -11,5 +13,17 @@ import javax.persistence.Table;
 @Data
 @Entity
 @Table(name = "dat_keyword")
-public class Keyword {
+public class Keyword extends SuperEntity {
+
+    private String name;
+
+    private String declare;
+
+    private  String keyword;
+
+    @Column(name="sub_send")
+    private Integer subSend;
+
+    @Column(name="media_id")
+    private String mediaId;
 }
