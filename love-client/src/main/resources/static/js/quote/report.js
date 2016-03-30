a66aeb4219d856492bd32237b9e9bc5128e1c90c
liyi <liyi.9ban@gmail.com>
2016-03-30 14:03:58 +0800

    add price module

---
+++ love-client/src/main/resources/static/js/quote/report.js
@@ -0,0 +1,15 @@
+///**
+// * Created by liyi on 2016/3/29.
+// */
+//$(function(){
+//    var shareData = {
+//        title : '全国4S店报价情报',
+//        desc : '全国4S店报价情报',
+//        link : getRootPath()
+//        + '/auth/check?scope=snsapi_base&target_uri=pages%2flottery%2fshare&redirect',
+//        img_url : getRootPath() + '/pages/lottery/images/share.png'
+//    };
+//    weixin.showOptionMenu();
+//    weixin.onMenuShareAppMessage(shareData);
+//    weixin.onMenuShareTimeline(shareData);
+//});
